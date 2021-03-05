2f60bf2762afa04b7a37fe61ef018ac43efe25da
kosei-kubota <kagesame1096@gmail.com>
2021-03-05 20:51:18 +0900

    orders_controllerのコントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,21 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
+  def order_params
+    params.require(:order).permit(:price)
+  end
 end
